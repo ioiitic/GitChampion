46be6d1872078538a31cae61feb44d0c180d26c3
unknown <baondforwork@gmail.com>
2022-05-26 19:10:48 +0700

    f4

--- demo.c
+++ demo.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+	 void f4(){
+	 	int x,y;
+	 	printf("Nhap 2 so a va b : \n");
+	 	scanf("%d",&x);
+	 	scanf("%d",&y);
+	 	printf(" tich cua 2 so la %ld",x*y);
+	 }
 int main()
 {
     printf("Hello F-Code:\n");
-    f1();
-    f2();
-    f3();
-    f4();
+   // f1();
+    //f2();
+   // f3();
+    f4();//
     return 0;
 }
